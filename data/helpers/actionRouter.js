cf35c5ce131c7f0f01155ef1513eaec1d1506424
shayaanakbar <shayaanakbar1106@gmail.com>
2019-05-18 12:12:40 -0500

    finished routes for actionrouter

--- data/helpers/actionRouter.js
+++ data/helpers/actionRouter.js
@@ -23,7 +23,37 @@ router.post('/', (req, res) => {
   })
 });
 
+router.put('/:id', (req, res) => {
+  const updateAction = req.body
+  const id = req.params.id
 
+  dbs.update(id, updateAction)
+    .then( action => {
+      res.status(200).json(action)
+    })
+    .catch(err => {
+      res.status(500).json({ error: err, message: "failed to update"});
+    })
+});
+
+router.delete('/:id', (req, res) => {
+  const actionid = req.params.id
+  dbs.remove(actionid)
+    .then(action => {
+      if(action) {
+        dbs.remove(action).then(
+          removeaction => {
+            res.status(201).json(removeaction)
+          }
+        )
+      } else {
+        res.status(404).json({error: err, message: "user w id doesnt exist"});
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: "the user could not be removed"});
+    })
+})
 
 
 module.exports = router;
