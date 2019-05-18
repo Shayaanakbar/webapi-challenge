e48465f0692d482a2ac364702c36a5ea92e63f54
shayaanakbar <shayaanakbar1106@gmail.com>
2019-05-18 12:52:31 -0500

    Fixing project/action errors

--- data/helpers/actionRouter.js
+++ data/helpers/actionRouter.js
@@ -1,57 +1,59 @@
 const express = require('express')
 const router = express.Router();
-const dbs = require('./actionModel');
+const dbaction = require('./actionModel');
 router.use(express.json());
 
 router.get('/', (req, res) =>{
-  dbs.get()
+  dbaction.get()
     .then( project => {
       res.status(200).json(project)
     })
     .catch( error => {
-      res.status(500).json({error:{message: "Error"}});
+      res.status(500).json({error:{message: "Couldn't get data"}})
     })
 });
 
 router.post('/', (req, res) => {
-  const newRoute = req.body
-  dbs.insert(newRoute)
-    .then( action => {
+  const newAction = req.body
+  dbaction.insert(newAction)
+    .then( action =>{
       res.status(200).json(action)
-    }).catch(error => {
-      res.status(500).json({error:{message: "Error"}});
+    }).catch( error => {
+    res.status(500).json({error:{message: "Could't get data"}})
   })
-});
+})
+
 
 router.put('/:id', (req, res) => {
-  const updateAction = req.body
+  const updateAct = req.body
   const id = req.params.id
 
-  dbs.update(id, updateAction)
+  dbaction.update(id, updateAct)
     .then( action => {
       res.status(200).json(action)
     })
-    .catch(err => {
-      res.status(500).json({ error: err, message: "failed to update"});
+    .catch( err => {
+      res.status(500).json({ error: err, message:"Could not update data"})
     })
-});
 
-router.delete('/:id', (req, res) => {
+})
+
+router.delete('/:id', (req, res)=>{
   const actionid = req.params.id
-  dbs.remove(actionid)
-    .then(action => {
-      if(action) {
-        dbs.remove(action).then(
+  dbaction.remove(actionid)
+    .then( action =>{
+      if(action){
+        dbaction.remove(actionid).then(
           removeaction => {
             res.status(201).json(removeaction)
           }
         )
-      } else {
-        res.status(404).json({error: err, message: "user w id doesnt exist"});
+      }else{
+        res.status(404).json({ error: err, mesage : "User does not exist"})
       }
     })
-    .catch(error => {
-      res.status(500).json({ message: "the user could not be removed"});
+    .catch(error =>{
+      res.status(500).json({  message: "User not be removed"})
     })
 })
 
