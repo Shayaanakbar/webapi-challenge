d1994eafba2ab0b1710373e215350edd8c3b5a50
shayaanakbar <shayaanakbar1106@gmail.com>
2019-05-20 19:33:35 -0500

    Deleted stretch

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const server = express();
 const actionRouter = require('./data/helpers/actionRouter.js');
 const projectRouter = require('./data/helpers/projectRouter.js');
 
-
 server.use(express.json());
 
 server.use('/api/actions', actionRouter)
