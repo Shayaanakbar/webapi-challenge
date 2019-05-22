dc437c0856bd3ccfcc8d02d7d513f68a69e89038
shayaanakbar <shayaanakbar1106@gmail.com>
2019-05-21 19:42:51 -0500

    SQLITE error

--- data/helpers/actionRouter.js
+++ data/helpers/actionRouter.js
@@ -3,29 +3,26 @@ const router = express.Router();
 const dbaction = require('./actionModel');
 router.use(express.json());
 
-router.get("/", async (req, res) => {
-  try {
-    const allActions = await dbaction.get();
-    res.json({ message: allActions });
-  } catch (err) {
-    res.status(500).json({ message: "Error: server conflict" });
-  }
+router.get('/', (req, res) =>{
+  dbaction.get()
+    .then( actions => {
+      res.status(200).json(actions)
+    })
+    .catch( err => {
+      res.status(500).json({ error: err, message:"Could not update data"})
+    })
 });
 
-router.post("/", async (req, res) => {
-  try {
-    const validateProject = await dbaction.get(req.body.project_id);
-
-    if (validateProject) {
-      const newAction = await dbaction.insert(req.body);
-      res.json({ message: newAction });
-    } else {
-      res.status(404).json({ message: "Invalid project id" });
-    }
-  } catch (err) {
-    res.status(500).json({ message: "Error: server conflict" });
-  }
-});
+router.post('/', (req, res) => {
+  const newAction = req.body
+  dbaction.insert(newAction)
+    .then( action =>{
+      res.status(200).json(action)
+    })
+    .catch( err => {
+      res.status(500).json({ error: err, message:"Could not update data"})
+  })
+})
 
 
 router.put('/:id', (req, res) => {
@@ -56,8 +53,8 @@ router.delete('/:id', (req, res)=>{
         res.status(404).json({ error: err, mesage : "User does not exist"})
       }
     })
-    .catch(error =>{
-      res.status(500).json({  message: "User not be removed"})
+    .catch( err => {
+      res.status(500).json({ error: err, message:"Could not update data"})
     })
 })
 
